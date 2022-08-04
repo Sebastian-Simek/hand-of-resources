20e92b3b0c158b2e74418870b340ae5aada00896
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-04 15:20:08 -0700

    created and passed first test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,25 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#GET / should return a list of instruments', async () => {
+    const res = await request(app).get('/instruments');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(expect.arrayContaining([
+      expect.objectContaining(
+        {
+          id: expect.any(String),
+          name: expect.any(String),
+          category: expect.any(String),
+          range: expect.any(String),
+        }
+      )
+    ]));
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,24 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS instruments;
+
+CREATE TABLE instruments (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    category VARCHAR NOT NULL,
+    range VARCHAR NOT NULL
+);
+
+INSERT INTO instruments (
+    name,
+    category,
+    range
+)
+VALUES 
+('Violin', 'Strings', 'Treble'),
+('Viola', 'Strings', 'Alto'),
+('Cello', 'Strings', 'Tenor'),
+('Bass', 'Strings', 'Bass'),
+('Trumpet', 'Brass', 'Treble'),
+('Bassoon', 'Woodwinds', 'Tenor'),
+('Piano', 'Percussion', 'All')
